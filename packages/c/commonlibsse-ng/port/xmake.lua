5f449f7198ebbc5a3c4a49dc7b9190eb70293b56
psych0v0yager <105936906+psych0v0yager@users.noreply.github.com>
2024-04-29 04:26:25 -0500

    Update xmake.lua

--- packages/c/commonlibsse-ng/port/xmake.lua
+++ packages/c/commonlibsse-ng/port/xmake.lua
@@ -10,10 +10,8 @@ set_languages("c++23")
 -- add rules
 add_rules("mode.debug", "mode.release")
 
--- set warnings
+-- set warnings and optimizations
 set_warnings("allextra", "error")
-
--- set optimization
 set_optimize("faster")
 
 -- add options
@@ -49,7 +47,7 @@ if has_config("skse_xbyak") then
     add_requires("xbyak")
 end
 
--- targets
+-- define targets
 target("CommonLibSSE")
     set_kind("static")
 
@@ -64,7 +62,7 @@ target("CommonLibSSE")
     add_options("skyrim_se", "skyrim_ae", "skyrim_vr", "skse_xbyak")
 
     -- add system links
-    add_syslinks("version", "user32", "shell32", "ole32", "advapi32")
+    add_syslinks("advapi32", "d3d11", "d3dcompiler", "dbghelp", "dxgi", "ole32", "shell32", "user32", "version")
 
     -- add source files
     add_files("src/**.cpp")
@@ -74,6 +72,7 @@ target("CommonLibSSE")
     add_headerfiles(
         "include/(RE/**.h)",
         "include/(REL/**.h)",
+        "include/(REX/**.h)",
         "include/(SKSE/**.h)"
     )
 
@@ -91,31 +90,33 @@ target("CommonLibSSE")
     add_cxxflags("cl::/we4715") -- `function` : not all control paths return a value
 
     -- disable warnings
-    add_cxxflags("cl::/wd4005") -- macro redefinition
-    add_cxxflags("cl::/wd4061") -- enumerator `identifier` in switch of enum `enumeration` is not explicitly handled by a case label
-    add_cxxflags("cl::/wd4068") -- unknown pragma 'clang'
-    add_cxxflags("cl::/wd4200") -- nonstandard extension used : zero-sized array in struct/union
-    add_cxxflags("cl::/wd4201") -- nonstandard extension used : nameless struct/union
-    add_cxxflags("cl::/wd4265") -- 'type': class has virtual functions, but its non-trivial destructor is not virtual; instances of this class may not be destructed correctly
-    add_cxxflags("cl::/wd4266") -- 'function' : no override available for virtual member function from base 'type'; function is hidden
-    add_cxxflags("cl::/wd4371") -- 'classname': layout of class may have changed from a previous version of the compiler due to better packing of member 'member'
-    add_cxxflags("cl::/wd4514") -- 'function' : unreferenced inline function has been removed
-    add_cxxflags("cl::/wd4582") -- 'type': constructor is not implicitly called
-    add_cxxflags("cl::/wd4583") -- 'type': destructor is not implicitly called
-    add_cxxflags("cl::/wd4623") -- 'derived class' : default constructor was implicitly defined as deleted because a base class default constructor is inaccessible or deleted
-    add_cxxflags("cl::/wd4625") -- 'derived class' : copy constructor was implicitly defined as deleted because a base class copy constructor is inaccessible or deleted
-    add_cxxflags("cl::/wd4626") -- 'derived class' : assignment operator was implicitly defined as deleted because a base class assignment operator is inaccessible or deleted
-    add_cxxflags("cl::/wd4710") -- 'function' : function not inlined
-    add_cxxflags("cl::/wd4711") -- function 'function' selected for inline expansion
-    add_cxxflags("cl::/wd4820") -- 'bytes' bytes padding added after construct 'member_name'
-    add_cxxflags("cl::/wd5082") -- second argument to 'va_start' is not the last named parameter
-    add_cxxflags("cl::/wd5026") -- 'type': move constructor was implicitly defined as deleted
-    add_cxxflags("cl::/wd5027") -- 'type': move assignment operator was implicitly defined as deleted
-    add_cxxflags("cl::/wd5045") -- compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
-    add_cxxflags("cl::/wd5053") -- support for 'explicit(<expr>)' in C++17 and earlier is a vendor extension
-    add_cxxflags("cl::/wd5105") -- macro expansion producing 'defined' has undefined behavior (workaround for older msvc bug)
-    add_cxxflags("cl::/wd5204") -- 'type-name': class has virtual functions, but its trivial destructor is not virtual; instances of objects derived from this class may not be destructed correctly
-    add_cxxflags("cl::/wd5220") -- 'member': a non-static data member with a volatile qualified type no longer implies that compiler generated copy / move constructors and copy / move assignment operators are not trivial
+    add_cxxflags(
+        "cl::/wd4005", -- macro redefinition
+        "cl::/wd4061", -- enumerator `identifier` in switch of enum `enumeration` is not explicitly handled by a case label
+        "cl::/wd4068", -- unknown pragma 'clang'
+        "cl::/wd4200", -- nonstandard extension used : zero-sized array in struct/union
+        "cl::/wd4201", -- nonstandard extension used : nameless struct/union
+        "cl::/wd4265", -- 'type': class has virtual functions, but its non-trivial destructor is not virtual; instances of this class may not be destructed correctly
+        "cl::/wd4266", -- 'function' : no override available for virtual member function from base 'type'; function is hidden
+        "cl::/wd4371", -- 'classname': layout of class may have changed from a previous version of the compiler due to better packing of member 'member'
+        "cl::/wd4514", -- 'function' : unreferenced inline function has been removed
+        "cl::/wd4582", -- 'type': constructor is not implicitly called
+        "cl::/wd4583", -- 'type': destructor is not implicitly called
+        "cl::/wd4623", -- 'derived class' : default constructor was implicitly defined as deleted because a base class default constructor is inaccessible or deleted
+        "cl::/wd4625", -- 'derived class' : copy constructor was implicitly defined as deleted because a base class copy constructor is inaccessible or deleted
+        "cl::/wd4626", -- 'derived class' : assignment operator was implicitly defined as deleted because a base class assignment operator is inaccessible or deleted
+        "cl::/wd4710", -- 'function' : function not inlined
+        "cl::/wd4711", -- function 'function' selected for inline expansion
+        "cl::/wd4820", -- 'bytes' bytes padding added after construct 'member_name'
+        "cl::/wd5082", -- second argument to 'va_start' is not the last named parameter
+        "cl::/wd5026", -- 'type': move constructor was implicitly defined as deleted
+        "cl::/wd5027", -- 'type': move assignment operator was implicitly defined as deleted
+        "cl::/wd5045", -- compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
+        "cl::/wd5053", -- support for 'explicit(<expr>)' in C++17 and earlier is a vendor extension
+        "cl::/wd5105", -- macro expansion producing 'defined' has undefined behavior (workaround for older msvc bug)
+        "cl::/wd5204", -- 'type-name': class has virtual functions, but its trivial destructor is not virtual; instances of objects derived from this class may not be destructed correctly
+        "cl::/wd5220" -- 'member': a non-static data member with a volatile qualified type no longer implies that compiler generated copy / move constructors and copy / move assignment operators are not trivial
+    )
 
     add_cxxflags("clang::-Wno-overloaded-virtual")
     add_cxxflags("clang::-Wno-delete-non-abstract-non-virtual-dtor")
